213965651827338d30571ff3b41d116243e8005f
BangPenthol05 <102308710+BangPenthol05@users.noreply.github.com>
2022-10-17 22:34:53 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,11 +4,12 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *PAYMENT ↓*
-_*Pulsa/Pulse(Telkomsel):*_ 082287219167
-_*Dana/Gopay:*_ 0895612153565
+_*Pulsa(Telkomsel):*_ 0822149931770
+_*Dana/Gopay/Ovo  :*_ 082149931770
+_*Saweria:*_ https://saweria.co/Bangpenthol/
 Setelah melakukan donasi kirim bukti pembayaran ke owner,Dengan melakukan donasi,kamu telah mensupport bot ini agar on 24 jam dan fast respon
 `
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/BangPenthol05/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m, { viewOnce: true })
 
