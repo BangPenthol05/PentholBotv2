7a82f21195e43bd4aba0f28be8bde765bbac9e52
BangPenthol05 <102308710+BangPenthol05@users.noreply.github.com>
2022-10-17 22:44:12 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/FahriAdison/Sad-Bot-New*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+let str = `*https://github.com/BangPenthol05/PentholBotv2*\Mampir ke IG jangan lupa @pnthll_\nSilahkan follow github saya dibawah ,Terimakasih`
 
 conn.sendButton(m.chat, str, null,null,[['Menu',`${usedPrefix}menu`]], m, { viewOnce: true})
 
